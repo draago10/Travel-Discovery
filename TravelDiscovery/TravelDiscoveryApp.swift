ad7f810a8a1a6da7d6dd225b5ccb91f37c9bddaa
Daniel Wendel <dwendel@additionfi.com>
2023-01-05 15:14:26 -0500

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Daniel on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
